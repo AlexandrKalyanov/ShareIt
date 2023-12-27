aea0b9d7fb88a580c43ec342427158a88d7ad1fd
a.kalyanov <av.kalyanov@yandex.ru>
2023-12-27 19:59:59 +0300

    fix 1 sprint 13

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -27,7 +27,7 @@ public class ItemController {
     @PostMapping
     public ItemCreateDto create(@NotNull @RequestHeader(headerUser) long userId,
                                 @Valid @RequestBody ItemCreateDto itemCreateDto) {
-        log.info("Icome DTO: {}, user ID: {}",itemCreateDto,userId);
+        log.info("Icome DTO: {}, user ID: {}", itemCreateDto, userId);
         return itemService.create(userId, itemCreateDto);
     }
 
@@ -37,23 +37,25 @@ public class ItemController {
                                 @PathVariable long itemId) {
         return itemService.update(userId, itemUpdateDto, itemId);
     }
+
     @GetMapping("/{itemId}")
     public ItemCreateDto findByItemId(@NotNull @RequestHeader(headerUser) long userId,
-                                      @PathVariable long itemId){
+                                      @PathVariable long itemId) {
 
-        return itemService.findByItemId(userId,itemId);
+        return itemService.findByItemId(userId, itemId);
 
     }
 
     @GetMapping
-    public List<ItemCreateDto> findAllByUser (@NotNull @RequestHeader(headerUser) long userId){
+    public List<ItemCreateDto> findAllByUser(@NotNull @RequestHeader(headerUser) long userId) {
         return itemService.findAllByUser(userId);
     }
+
     @GetMapping("/search")
-    public List<ItemCreateDto> searchItems (@NotNull @RequestHeader(headerUser) long userId,
-                                            @NotNull @RequestParam String text){
-        log.info("Params: user id: {}, text: {}",userId,text);
-        return itemService.searchItems(userId,text);
+    public List<ItemCreateDto> searchItems(@NotNull @RequestHeader(headerUser) long userId,
+                                           @NotNull @RequestParam String text) {
+        log.info("Params: user id: {}, text: {}", userId, text);
+        return itemService.searchItems(userId, text);
     }
 
 
