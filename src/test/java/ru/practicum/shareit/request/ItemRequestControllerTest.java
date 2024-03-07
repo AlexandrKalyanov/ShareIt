bd6fa42ad10d3935f7bb550a7e11fedd5d6d2721
a.kalyanov <av.kalyanov@yandex.ru>
2024-03-07 14:47:54 +0300

    sprint 15 add some test to booking

---
+++ src/test/java/ru/practicum/shareit/request/ItemRequestControllerTest.java
@@ -0,0 +1,24 @@
+package ru.practicum.shareit.request;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemRequestControllerTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void findAllByUserId() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findRequest() {
+    }
+}
