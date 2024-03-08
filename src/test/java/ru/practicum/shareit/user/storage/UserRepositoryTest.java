2bf848c31e59de9cd5a6d2f41f7939490357fb8d
a.kalyanov <av.kalyanov@yandex.ru>
2024-03-09 02:34:36 +0300

    sprint 15 add some test

--- src/test/java/ru/practicum/shareit/user/storage/UserRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/user/storage/UserRepositoryTest.java
@@ -1,13 +1,10 @@
 package ru.practicum.shareit.user.storage;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 import ru.practicum.shareit.exceptions.ObjectNotFoundException;
-import ru.practicum.shareit.user.model.User;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,24 +13,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class UserRepositoryTest {
     @Autowired
     UserRepository userRepository;
-    User user;
 
-    @BeforeEach
-    void addUser() {
-        user = new User(null, "Alex", "Alex@mail.ru");
-        userRepository.save(user);
-    }
-
-    @AfterEach
-    void deleteALl() {
-        userRepository.deleteAll();
-    }
-
-    @Test
-    void getUserOrException_whenUserFound() {
-        User user1 = userRepository.getUserOrException(1L);
-        assertEquals(user, user1);
-    }
 
     @Test
     void getUserOrException_whenUserNotFound() {
